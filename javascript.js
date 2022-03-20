eb2deda4cc98460951ac993dd8af057265e1340f
bofmar <bofmar@outlook.com>
2022-03-20 20:46:39 +0200

    Add canvas functionality to the button
    Upon pressing the button we now generate the canvas with html2canvas. We also neeeded to add a function to replace the textarea with a pre to retain the proper formatting of the card

--- javascript.js
+++ javascript.js
@@ -1,3 +1,25 @@
-html2canvas(document.querySelector("form")).then(canvas => {
-  document.body.appendChild(canvas)
+
+const submitButton = document.querySelector("button");
+
+submitButton.addEventListener("click", (e)=>{
+  e.target.classList.add("invisible");
+  replaceTextArea();
+  // console.log(newDiv.innerHTML);
+  html2canvas(document.querySelector("form")).then(canvas => {
+    document.body.appendChild(canvas)
+  });  
 });
+
+//Because of limitations in the way html2canvas handles
+//textarea elements, we need to replace the textarea with
+//a pre element on runtime to keep its' formatting.
+function replaceTextArea(){
+  const textAreaDiv = document.querySelector("#message");
+  const textArea = document.querySelector("#msg");
+  const newDiv = document.createElement("pre");
+
+  newDiv.innerHTML = textArea.value;
+  newDiv.classList.add("textarea-copy");
+  textAreaDiv.appendChild(newDiv);
+  textAreaDiv.removeChild(textArea);
+}
