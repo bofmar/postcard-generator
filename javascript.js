d42732e3d21073f82de4e6dc45766e46a067b12b
bofmar <bofmar@outlook.com>
2022-03-21 14:00:11 +0200

    Make the form usable again after canvas is drawn

--- javascript.js
+++ javascript.js
@@ -1,25 +1,32 @@
 
 const submitButton = document.querySelector("button");
+const textAreaDiv = document.querySelector("#message");
+const textArea = document.querySelector("#msg");
+const newElement = document.createElement("pre");
 
 submitButton.addEventListener("click", (e)=>{
   e.target.classList.add("invisible");
   replaceTextArea();
-  // console.log(newDiv.innerHTML);
   html2canvas(document.querySelector("form")).then(canvas => {
     document.body.appendChild(canvas)
-  });  
+  });
+  e.target.classList.remove("invisible");
+  resetTextArea();
 });
 
 //Because of limitations in the way html2canvas handles
 //textarea elements, we need to replace the textarea with
 //a pre element on runtime to keep its' formatting.
 function replaceTextArea(){
-  const textAreaDiv = document.querySelector("#message");
-  const textArea = document.querySelector("#msg");
-  const newDiv = document.createElement("pre");
-
-  newDiv.innerHTML = textArea.value;
-  newDiv.classList.add("textarea-copy");
-  textAreaDiv.appendChild(newDiv);
+  newElement.innerHTML = textArea.value;
+  newElement.classList.add("textarea-copy");
+  textAreaDiv.appendChild(newElement);
   textAreaDiv.removeChild(textArea);
+}
+
+//after the canvas is drawn, we revert the changes to the DOM
+//to allow the user to change his inputs
+function resetTextArea(){
+  textAreaDiv.removeChild(newElement);
+  textAreaDiv.appendChild(textArea);
 }
